60d637efab9e5418eee3c29d64c7b01201e6c294
Yeray Candel <60876148+ycansam@users.noreply.github.com>
2022-11-22 20:07:45 +0100

    añadido sistema de rotacion manual

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -14,32 +14,65 @@ public class CubeController : MonoBehaviour
 
     [SerializeField]
     private float rotationSpeed = 1f;
-
-    private float rotateAngleHorizontal = 90f;
-
-    void Start() { }
+    bool rotating = false;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && !rotating)
         {
-            StartCoroutine(RotateRight());
+            StartCoroutine(RotateEase(rotationSpeed, Vector3.back * 90f));
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && !rotating)
+        {
+            StartCoroutine(RotateEase(rotationSpeed, Vector3.back * -90f));
+        }
+        if (Input.GetKeyDown(KeyCode.R) && !rotating)
+        {
+            StartCoroutine(RotateEase(rotationSpeed, new Vector3(90f, 0f, 0f)));
+        }
+        if (Input.GetKeyDown(KeyCode.F) && !rotating)
+        {
+            StartCoroutine(RotateEase(rotationSpeed, new Vector3(-90f, 0f, 0f)));
         }
     }
 
-    IEnumerator RotateRight()
+    void GetPlayerInputs() { }
+
+    IEnumerator Rotate(float duration)
     {
-        while (transform.rotation.y < rotateAngleHorizontal)
+        float startRotation = transform.eulerAngles.z;
+        float endRotation = startRotation + 90.0f;
+        float t = 0.0f;
+        while (t < duration)
         {
-            transform.localRotation = Quaternion.Slerp(
-                transform.rotation,
-                Quaternion.AngleAxis(rotateAngleHorizontal, Vector3.forward),
-                rotationSpeed * Time.deltaTime
+            t += Time.deltaTime;
+            float yRotation = Mathf.Lerp(startRotation, endRotation, t / duration) % 360.0f;
+            transform.eulerAngles = new Vector3(
+                transform.eulerAngles.x,
+                transform.eulerAngles.y,
+                yRotation
             );
             yield return null;
         }
-        transform.localRotation = Quaternion.Euler(0, rotateAngleHorizontal, 0);
-        rotateAngleHorizontal = rotateAngleHorizontal + 90f;
-        yield return null;
+    }
+
+    IEnumerator RotateEase(float duration, Vector3 direction)
+    {
+        var startRotation = this.transform.rotation;
+        var endRotation = transform.rotation * Quaternion.Euler(direction);
+        float t = 0.0f;
+        rotating = true;
+        float rate = 1.0f / duration;
+        while (t < 1f)
+        {
+            t += Time.deltaTime * rate;
+            transform.rotation = Quaternion.Slerp(
+                startRotation,
+                endRotation,
+                Mathf.SmoothStep(0.0f, 1.0f, t)
+            );
+            yield return null;
+        }
+        rotating = false;
     }
 }
