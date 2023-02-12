9483c73008cab1afa79ec2d1d244294c9553a08e
Yeray Candel <ycansam@epsg.upv.es>
2023-02-12 13:30:03 +0100

    fixed teleport on portal pass

--- Assets/Scripts/Portals/PortalTeleport.cs
+++ Assets/Scripts/Portals/PortalTeleport.cs
@@ -7,16 +7,24 @@ public class PortalTeleport : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private Transform otherPortal;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag(Tags.PLAYER).transform;
+    }
 
     private void OnTriggerStay(Collider other)
     {
 
         if (other.tag == Tags.PLAYER)
         {
+            Vector3 playerFromPortal = transform.position - player.position;
+            Debug.Log(playerFromPortal);
 
-            Transform player = other.transform;
-            Vector3 playerFromPortal = transform.InverseTransformPoint(player.position);
+            // player.transform.position = otherPortal.position - new Vector3(playerFromPortal.x, playerFromPortal.y, playerFromPortal.z);
 
         }
     }
+
 }
