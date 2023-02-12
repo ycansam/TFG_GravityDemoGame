f010cd5420b388c76d1517093cba7d8838a3ef35
Yeray Candel <ycansam@epsg.upv.es>
2023-02-12 13:38:43 +0100

    fixed portal teleport correctly on each side and no clipping

--- Assets/Scripts/Portals/PortalTeleport.cs
+++ Assets/Scripts/Portals/PortalTeleport.cs
@@ -7,6 +7,8 @@ public class PortalTeleport : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private Transform otherPortal;
+    [SerializeField]
+    private bool neg;
     private Transform player;
 
     private void Start()
@@ -19,10 +21,19 @@ public class PortalTeleport : MonoBehaviour
 
         if (other.tag == Tags.PLAYER)
         {
-            Vector3 playerFromPortal = transform.position - player.position;
-            Debug.Log(playerFromPortal);
 
-            // player.transform.position = otherPortal.position - new Vector3(playerFromPortal.x, playerFromPortal.y, playerFromPortal.z);
+            Vector3 playerFromPortal = transform.position - player.position;
+            Debug.Log(playerFromPortal.x);
+            if (!neg)
+            {
+                if (playerFromPortal.x <= -0.02)
+                    player.transform.position = otherPortal.position - new Vector3(playerFromPortal.x, playerFromPortal.y, playerFromPortal.z);
+            }
+            else
+            {
+                if (playerFromPortal.x >= -0.02)
+                    player.transform.position = otherPortal.position - new Vector3(playerFromPortal.x, playerFromPortal.y, playerFromPortal.z);
+            }
 
         }
     }
