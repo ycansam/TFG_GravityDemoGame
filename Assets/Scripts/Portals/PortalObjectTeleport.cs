01501c684640fb1174aceb9285ef3c2dd0b69223
Yeray Candel <ycansam@epsg.upv.es>
2023-03-26 13:22:00 +0200

    solucionado de verdad

--- Assets/Scripts/Portals/PortalObjectTeleport.cs
+++ Assets/Scripts/Portals/PortalObjectTeleport.cs
@@ -12,27 +12,22 @@ public class PortalObjectTeleport : MonoBehaviour
     [SerializeField]
     private bool neg;
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         GameObject objectToTp = other.gameObject;
         if (other.tag != Tags.PLAYER)
         {
 
-            float distance = 0.2f;
             Vector3 playerFromPortal = transform.position - objectToTp.transform.position;
             // si es negativo 
             if (!neg)
             {
-                if (playerFromPortal.x <= -distance)
-                {
-                    objectToTp.transform.position = otherPortal.position - new Vector3(playerFromPortal.x, playerFromPortal.y, playerFromPortal.z);
-                }
+                Debug.Log(playerFromPortal);
+                objectToTp.transform.position = otherPortal.position - new Vector3(playerFromPortal.x, playerFromPortal.y, playerFromPortal.z);
             }
             else
             {
-                //  || playerFromPortal.y >= -distance || playerFromPortal.z >= -distance
-                if (playerFromPortal.x >= -distance)
-                    objectToTp.transform.position = otherPortal.position - new Vector3(playerFromPortal.x, playerFromPortal.y, playerFromPortal.z);
+                objectToTp.transform.position = otherPortal.position - new Vector3(playerFromPortal.x, playerFromPortal.y, playerFromPortal.z);
             }
         }
     }
