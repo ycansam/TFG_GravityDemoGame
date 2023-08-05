7d3e227452057c5bd4952c1e69cabf6f9b3b23be
Yeray Candel <ycansam@epsg.upv.es>
2023-08-05 19:07:19 +0200

    level 4 preparado

--- Assets/Scenes/Scripts/LevelManager4.cs
+++ Assets/Scenes/Scripts/LevelManager4.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelTestManager : MonoBehaviour
+public class LevelManager4 : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         PlayerPhone.SetPlayerPhoneOn();
