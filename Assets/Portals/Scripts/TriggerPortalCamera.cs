ae095339aae27d61befde7297e5fd7a0828ca8e7
Yeray Candel <ycansam@epsg.upv.es>
2023-08-12 12:34:27 +0200

    arreglado vision de los objetos cuando el jugador esta en otro cubo

--- Assets/Portals/Scripts/TriggerPortalCamera.cs
+++ Assets/Portals/Scripts/TriggerPortalCamera.cs
@@ -7,30 +7,64 @@ public class TriggerPortalCamera : MonoBehaviour
     [SerializeField]
     Transform cam;
 
+    private List<GameObject> includedObjects = new List<GameObject>();
+
+
+    private void IncludeMovableIfNotIncluded(GameObject ob)
+    {
+        if (!includedObjects.Contains(ob))
+        {
+            includedObjects.Add(ob);
+        }
+    }
+    private void RemoveMovableIfIncluded(GameObject ob)
+    {
+        if (includedObjects.Contains(ob))
+        {
+            int index = includedObjects.IndexOf(ob);
+            includedObjects.RemoveAt(index);
+        }
+    }
+
+    private void DisableObjectRenders()
+    {
+        foreach (GameObject ob in includedObjects)
+        {
+            ob.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    private void EnableObjectRenders()
+    {
+        foreach (GameObject ob in includedObjects)
+        {
+            ob.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {
+        if (other.tag == Tags.OBJECT_MOVABLE_TAG)
+        {
+            IncludeMovableIfNotIncluded(other.gameObject);
+        }
+
         if (other.name == cam.name)
         {
-            // Hide();
+            DisableObjectRenders();
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        if (other.tag == Tags.OBJECT_MOVABLE_TAG)
+        {
+            RemoveMovableIfIncluded(other.gameObject);
+        }
+
         if (other.name == cam.name)
         {
-            // Show();
+            EnableObjectRenders();
         }
     }
-
-    // private void Show()
-    // {
-    //      cam.GetComponent<Camera>().cullingMask |= 1 << LayerMask.NameToLayer("ObjectMovable");
-    // }
-
-    // // Turn off the bit using an AND operation with the complement of the shifted int:
-    // private void Hide()
-    // {
-    //      cam.GetComponent<Camera>().cullingMask &= ~(1 << LayerMask.NameToLayer("ObjectMovable"));
-    // }
 }
